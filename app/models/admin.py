9f3258a2af8ccf11b207d3811d326a124657cf22
rakibhossendev <rh01828315879@gmail.com>
2026-07-23 23:04:13 +0600

    Revert "Merge pull request #17 from Srestho0101/main"
    This reverts commit eb58824a8689eb7d2fcbb000e383186df1eddabd, reversing
    changes made to c0fa45a338abe084fc5780f29a78b622ebb24e6b.

--- app/models/admin.py
+++ app/models/admin.py
@@ -1,10 +1,10 @@
 from app.extensions import db
-# class Admin(db.Model):
-#     __tablename__ = "admin"
+class Admin(db.Model):
+    __tablename__ = "admin"
 
-#     id = db.Column(db.Integer, primary_key = True)
-#     username = db.Column(db.String(50),nullable=False,unique=True)
-#     password = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50),nullable=False,unique=True)
+    password = db.Column(db.String(120), nullable=False)
 
 class PrincipalDataInfo(db.Model):
     __tablename__ = "principal_info"
